e0ab0ea24f4136feb8916687acbdd94aed0edb72
NewbieCreate <wkdtk85@daum.net>
2025-10-01 13:53:33 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13852 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,6 +1,5 @@
-public class Main{
-    public static void main(String[] args)
-    {
-        System.out.printf("Hello World!");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
