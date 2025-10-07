6d7a71be22508aa06d328834d0cb4cc1d8139ca0
NewbieCreate <wkdtk85@daum.net>
2025-10-07 17:42:53 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        
+        long sum = a + b + c;
+        
+        bw.write(String.valueOf(sum));
+        bw.flush();
+    }
+}
